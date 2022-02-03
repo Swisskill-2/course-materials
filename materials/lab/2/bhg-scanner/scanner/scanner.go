a474676a15eb56c90469218ee7c30fce0f7851a0
Swisskill-2 <98115027+Swisskill-2@users.noreply.github.com>
2022-02-03 10:16:22 -0700

    CodeAnywhere appears broken

--- materials/lab/2/bhg-scanner/scanner/scanner.go
+++ materials/lab/2/bhg-scanner/scanner/scanner.go
@@ -1,7 +1,12 @@
 // bhg-scanner/scanner.go modified from Black Hat Go > CH2 > tcp-scanner-final > main.go
 // Code : https://github.com/blackhat-go/bhg/blob/c27347f6f9019c8911547d6fc912aa1171e6c362/ch-2/tcp-scanner-final/main.go
 // License: {$RepoRoot}/materials/BHG-LICENSE
-// Useage:
+// Useage: This is a port scanner. The steps for running are: cd workspace/course-materials/materials/lab/2/bhg-scanner/main
+//															  go build
+//															  time ./main
+// You can also test it by: cd workspace/course-materials/materials/lab/2/bhg-scanner/scanner
+//							go test
+// Will Brant, 2/3/22
 // {TODO 1: FILL IN}
 
 package scanner
@@ -15,14 +20,12 @@ import (
 
 //TODO 3 : ADD closed ports; currently code only tracks open ports
 var openports []int // notice the capitalization here. access limited!
-var closports []int
+var closports []int 
 
 func worker(ports, results chan int) {
 	for p := range ports {
 		address := fmt.Sprintf("scanme.nmap.org:%d", p)
 		conn, err := net.DialTimeout("tcp", address, 1*time.Second) // TODO 2 : REPLACE THIS WITH DialTimeout (before testing!)
-		//jazz := fmt.Sprintf("\n%d", p)
-		//fmt.Println(jazz)
 		if err != nil {
 			results <- 0
 			continue
@@ -37,9 +40,9 @@ func worker(ports, results chan int) {
 // med: easy + return  complex data structure(s?) (maps or slices) containing the ports.
 // hard: restructuring code - consider modification to class/object
 // No matter what you do, modify scanner_test.go to align; note the single test currently fails
-func PortScanner() int {
+func PortScanner() (string, int) {
 
-	ports := make(chan int, 100) // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
+	ports := make(chan int, 1024) // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE (am i doing this right?)
 	results := make(chan int)
 
 	for i := 0; i < cap(ports); i++ {
@@ -56,19 +59,25 @@ func PortScanner() int {
 		port := <-results
 		if port != 0 {
 			openports = append(openports, port)
+		} else {
+			closports = append(closports, port)
 		}
 	}
 
 	close(ports)
 	close(results)
 	sort.Ints(openports)
-
+	
 	//TODO 5 : Enhance the output for easier consumption, include closed ports
 
 	for _, port := range openports {
 		fmt.Printf("%d open\n", port)
+	
 	}
-
-	return len(openports) // TODO 6 : Return total number of ports scanned (number open, number closed);
+	return fmt.Sprintf("There are %d ports open and %d ports closed", len(openports), closports), len(openports)
+	//return len(openports) 
+	//return len(closports)
+	
+	// TODO 6 : Return total number of ports scanned (number open, number closed);
 	//you'll have to modify the function parameter list in the defintion and the values in the scanner_test
 }
